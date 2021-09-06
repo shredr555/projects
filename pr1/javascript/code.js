fb4aa13760e7914ff03317ea3fe972b65766231c
shredr555 <65497900+shredr555@users.noreply.github.com>
2021-09-06 13:14:56 +0300

    Add files via upload

---
+++ pr1/javascript/code.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 4,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplaySpeed: 2500,
+    });
+});
